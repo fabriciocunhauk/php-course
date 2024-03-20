33e0d9947bc18b8ef9cd7719d8a64f6188a46cde
fabriciocunhauk <fabriciocunhadeveloper@gmail.com>
2024-03-20 17:45:31 +0000

    Exercise 3

---
+++ ex003/index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Primitive types in PHP</title>
+</head>
+
+<body>
+    <h1>Primitive types test</h1>
+    <?php
+    // 0x = hexadecimal 0b = binario 0 = Octal
+    // $num = 10;
+    // echo "The variable value is 0x" . $num;
+    // echo "The variable value is 0x $num";
+
+    // $v = "Fabricio";
+    // var_dump($v);
+
+    // $num = (int) 3e2; // 3 x 10(2)
+    // echo "The variable value is $num";
+    // var_dump($v);
+
+    // $num = (float) "950";
+    // var_dump($num);
+
+    // $casado = true;
+    // var_dump($casado);
+    // print "The variable value is $casado";
+
+    // $arr = [1, 2, 3, 4, 5, 6.8];
+    // var_dump($arr);
+
+    class Person
+    {
+        private string $name;
+    }
+
+    $p = new Person;
+    var_dump($p);
+    ?>
+</body>
+
+</html>
