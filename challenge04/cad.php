3ab7adb14ded1c12647822d881cb8a7d2f177cb2
fabriciocunhauk <fabriciocunhadeveloper@gmail.com>
2024-03-21 15:54:50 +0000

    Challenge 04

--- challenge04/cad.php
+++ challenge04/cad.php
@@ -14,8 +14,15 @@
     </header>
     <main>
         <?php
+        $startDate = date("m-d-Y", strtotime("-7 days"));
+        $endDate = date("m-d-Y");
+
+        $url = 'https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarPeriodo(dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?@dataInicial=\'' . $startDate . '\'&@dataFinalCotacao=\'' . $endDate . '\'&$top=1&$orderby=dataHoraCotacao%20desc&$format=json&$select=cotacaoCompra';
+
+        $data = json_decode(file_get_contents($url), true);
+
         $real = $_GET['number'] ?? "No Number";
-        $dolarPrice = 5.22;
+        $dolarPrice = $data["value"][0]["cotacaoCompra"];
         $convertedNumber = $real / $dolarPrice;
 
         echo "Your " . number_format($real, 2, ",", ".") . " Converted to Dolar is  US$" . number_format($convertedNumber, 2, ",", ".");
